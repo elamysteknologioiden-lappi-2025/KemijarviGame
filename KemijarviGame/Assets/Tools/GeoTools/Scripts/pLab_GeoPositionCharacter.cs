b3039c22f21a0eee4281f75fd9e20dc377d4c62a
Arto Söderström <arto.soderstrom@lapinamk.fi>
2019-12-10 10:03:59 +0200

    Modify character movement. Modify animations. Organize project files. Light map stuff.

--- KemijarviGame/Assets/Tools/GeoTools/Scripts/pLab_GeoPositionCharacter.cs
+++ KemijarviGame/Assets/Tools/GeoTools/Scripts/pLab_GeoPositionCharacter.cs
@@ -39,7 +39,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.XR;
 
 
@@ -75,9 +74,33 @@ public class pLab_GeoPositionCharacter : MonoBehaviour {
 
     private const string ANIMATOR_SPEED_PARAMETER = "MovementSpeed";
 
+    private float startTime = 0f;
+    private Vector3 startPosition = Vector3.zero;
+    private Vector3 desiredPos = Vector3.zero;
+    private Vector3 changePos = Vector3.zero;
+    private Vector3 newPos = Vector3.zero;
+    private float speed = 0;
+    private float timeDif = 0;
+    private float distance = 0;
+
+    [SerializeField]
+    private Animator characterAnimator;
+
+    private Transform characterTransform;
+
+    bool initialPositioningDone = false;
+
+
+
     #endregion
 
     #region // From Base Class
+
+    void Awake() {
+        if (characterTransform == null) {
+            characterTransform = characterAnimator.gameObject.transform;
+        }
+    }
 
     void Start() {
         objectTransform = transform;
@@ -111,105 +134,62 @@ public class pLab_GeoPositionCharacter : MonoBehaviour {
         }
     }
 
-    public Text pos;
-    private float startTime = 0f;
-    private Vector3 startPosition = Vector3.zero;
-    private Vector3 desiredPos = Vector3.zero;
-    private Vector3 changePos = Vector3.zero;
-    public Vector3 hitPos;
-    private float speed = 0;
-
-    [SerializeField]
-    private Animator characterAnimator;
-    private float timeDif = 0;
-    private float distance = 0;
-    private Vector3 lastPos = new Vector3();
-
-    private Vector3 newPos = Vector3.zero;
-
-
-    float timme;
-
     void Update() {
-
-        // #if UNITY_EDITOR
-        
-        // if (Input.GetMouseButtonDown(0))
-        // {
-        //     Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        //     RaycastHit hit;
-        //     if (Physics.Raycast(ray, out hit, 1000f))
-        //     {
-        //         pLab_NPC npc = hit.collider.gameObject.GetComponent<pLab_NPC>();
-
-        //         if (null != npc)
-        //         {
-        //             npc.ActivateNPC();
-        //         }
-        //     }
-        // }
-
-        // #endif
-
-        // newPos = new Vector3((float)( pLab_GeoLocation.instance.UtmX - pLab_GeoMap.instance.UtmX),
-        //     0f, (float)(pLab_GeoLocation.instance.UtmY - pLab_GeoMap.instance.UtmY));
 
         // If moved, compute new pos;
         if (Vector3.Distance(newPos, desiredPos) > 0.1f) {
 
+            desiredPos = newPos;
+            startPosition = transform.position;
+
             // Compute new position distance
-            distance = Vector3.Distance(newPos, transform.position);
+            distance = Vector3.Distance(startPosition, desiredPos);
 
-            // Time last update
-            timeDif = Time.time - startTime;
-
-            if(timeDif > 0.5f){
-                timeDif = 0.5f;
+            if (distance > 0) {
+                speed = Mathf.Clamp(distance, 0.05f, 25f);
+            } else {
+                speed = 0;
             }
 
-            speed = distance / timeDif;
-            speed = speed * 0.8f;
-            if (speed < 0.05f){
-                speed = 0.05f;
-            }
+            characterAnimator.SetFloat(ANIMATOR_SPEED_PARAMETER, speed);
 
             startTime = Time.time;
 
-            startPosition = transform.position;
-            desiredPos = newPos;
-            timme = 0;
+            //Calculate and update the angle
+            Vector3 targetDir = desiredPos - startPosition;
+            float angle = Vector3.Angle(targetDir, transform.forward);
+            Vector3 localRot = characterTransform.localEulerAngles;
 
-            // Debug.LogError(startPosition + " speed" + speed);
+            localRot.y = angle;
+
+            if (targetDir.x < 0)
+                localRot.y *= -1;
+
+            characterTransform.localEulerAngles = localRot;
         }
 
-        
-        float distCovered = (Time.time - startTime) * speed;
-        float fracJourneya = distCovered / distance;
+        if (distance > 0) {
+            float distCovered = (Time.time - startTime) * speed;
+            // Debug.Log($"Dist before: {distCovered}, after: {distCoveredVector}");
+            
+            float distanceCoveredRatio = distCovered / distance;
 
-        if(speed == 0)
-        {
-            fracJourneya = 1;
+            if(speed == 0)
+            {
+                distanceCoveredRatio = 1;
+            }
+
+            if(distanceCoveredRatio > 1)
+            {
+                distanceCoveredRatio = 1;
+                speed = 0;
+            }
+
+            characterAnimator.SetFloat(ANIMATOR_SPEED_PARAMETER, speed);
+
+
+            transform.position = Vector3.Lerp(startPosition, desiredPos, distanceCoveredRatio);
         }
-
-        if(fracJourneya > 1)
-        {
-            speed = 0;
-        }
-
-        // transform.position = desiredPos;
-        timme += Time.deltaTime;
-        characterAnimator.SetFloat(ANIMATOR_SPEED_PARAMETER, speed);
-
-        transform.position = Vector3.Lerp(startPosition, desiredPos, fracJourneya);
-
-        Vector3 targetDir = newPos - startPosition;
-        float angle = Vector3.Angle(targetDir, transform.forward);
-        Vector3 localRot = characterAnimator.gameObject.transform.localEulerAngles;
-        localRot.y = angle;
-        if (targetDir.x < 0)
-            localRot.y *= -1;
-        characterAnimator.gameObject.transform.localEulerAngles = localRot;
-
     }
 
 
@@ -218,7 +198,7 @@ public class pLab_GeoPositionCharacter : MonoBehaviour {
     #region // Private Functions
 
     /// <summary>
-    /// Event handler for OnLocationUpdated-event
+    /// Event handler for OnLocationUpdated-event. Calculates new position for the character
     /// </summary>
     /// <param name="sender"></param>
     /// <param name="e"></param>
@@ -227,10 +207,19 @@ public class pLab_GeoPositionCharacter : MonoBehaviour {
         CalculateNewPosition(e.location);
     }
 
+    /// <summary>
+    /// Calculates characters new position from LatLon coordinates
+    /// </summary>
+    /// <param name="latLon"></param>
     private void CalculateNewPosition(pLab_LatLon latLon) {
         pLab_UTMCoordinates coordinates = new pLab_UTMCoordinates(latLon);
         newPos = new Vector3((float)(coordinates.UTMX - pLab_GeoMap.instance.UtmX),
             0f, (float)(coordinates.UTMY - pLab_GeoMap.instance.UtmY));
+
+        if (!initialPositioningDone) {
+            transform.position = newPos;
+            initialPositioningDone = true;
+        }
     }
 
     #endregion
@@ -241,24 +230,24 @@ public class pLab_GeoPositionCharacter : MonoBehaviour {
     #region // Public Functions
 
 
-    public void ChangeMoveType(Slider slider) {
-        geoCharacterMoveType = (GeoCharacterMoveType)slider.value;
-    }
+    // public void ChangeMoveType(Slider slider) {
+    //     geoCharacterMoveType = (GeoCharacterMoveType)slider.value;
+    // }
 
-    /// <summary>
-    /// SetObjectToPosition
-    /// </summary>
-    /// <param name="aPos"></param>
-    public void SetObjectToPosition(Vector2 aPos) {
+    // /// <summary>
+    // /// SetObjectToPosition
+    // /// </summary>
+    // /// <param name="aPos"></param>
+    // public void SetObjectToPosition(Vector2 aPos) {
 
-        Ray ray = Camera.main.ScreenPointToRay(aPos);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 1000f)) {
-           // objectTransform.position = hit.point;
-        }
-        hitPos = hit.point;
+    //     Ray ray = Camera.main.ScreenPointToRay(aPos);
+    //     RaycastHit hit;
+    //     if (Physics.Raycast(ray, out hit, 1000f)) {
+    //        // objectTransform.position = hit.point;
+    //     }
+    //     hitPos = hit.point;
 
-    }
+    // }
 
     #endregion
 
